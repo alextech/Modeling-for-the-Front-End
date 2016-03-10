dc482eec499045bc7a000dbcfc9f676713b87394
Alexander Romanenko <alex@alex-tech-adventures.com>
2016-03-10 11:37:02 -0500

    inject HTML directly from AJAX

--- catalogue.js
+++ catalogue.js
@@ -5,37 +5,10 @@ var Catalogue = {
 
     load: function() {
         $.ajax({
-            url: 'issues.json',
+            url: 'issues.html',
             type: 'get',
-            dataType: 'json',
             success: function(data, textStatus, jqXHR) {
-                console.log(data);
-                var html = '<div class="row">';
-                for(var i = 0; i < data.issues.length; i++) {
-                    var issue = data.issues[i];
-                    console.log(issue);
-
-                    var priorityClass;
-                    if(issue.priority.localeCompare("high") == 0) {
-                        priorityClass = "btn-danger"
-                    } else if(issue.priority.localeCompare("medium") == 0) {
-                        priorityClass = "btn-warning"
-                    } else if(issue.priority.localeCompare("low") == 0) {
-                        priorityClass = "btn-info"
-                    } else {
-                        priorityClass = "btn-default"
-                    }
-
-                    html += '<div class="col-md-4">' +
-                                '<h2>'+issue.title+'</h2>' +
-                                '<p id="'+issue.id+'DescriptionParagraph">'+issue.description+'</p>' +
-                                '<p><a class="btn '+priorityClass+' moreDetails" href="#" role="button" data-issueID="'+issue.id+'" data-toggle="modal" data-target="#detailsModal">View details &raquo;</a></p>' +
-                            '</div>';
-
-                }
-                html += '</div>';
-
-                $("#issuesContainer").html(html);
+                $("#issuesContainer").html(data);
 
 
                 this.registerDetailsModal();
